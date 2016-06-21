a34c0a658e470cf1371dd606b05ab48657844241
Joel Pan <spamtastic@ketsugi.com>
2016-06-21 22:35:41 +0800

    Add main JS file

---
+++ index.js
@@ -0,0 +1,20 @@
+// Import the Plex API Headers library (https://www.npmjs.com/package/plex-api-headers)
+const plexHeaders = require('plex-api-headers');
+
+// Set defaults as constants
+const PLEX_SERVER_HOST = 'localhost';
+const PLEX_SERVER_PORT = 32400;
+
+// Define the main class
+class Plexacious {
+  /**
+   * Instantiate Plexacious object
+   *
+   * @param {object} config - An object containing all the necessary configuration to connect to the Plex server
+   */
+  constructor(config = {}) {
+
+  }
+}
+
+module.exports = Plexacious;
