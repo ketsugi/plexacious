b98db4ad0e627c6a5326e8cabfde1e71e4bfdf2e
Joel Pan <spamtastic@ketsugi.com>
2016-06-25 13:45:24 +0800

    Fix events() call to match renamed function eventFunctions()

--- test/plexacious.js
+++ test/plexacious.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
 const Plexacious = require('../lib/Plexacious');
+const env = require('node-env-file');
+env('./.env');
 const config = {
   "hostname": process.env.PLEX_SERVER_HOST,
   "port": process.env.PLEX_SERVER_PORT,
@@ -12,14 +14,14 @@ const plex = new Plexacious(config);
 describe('Plexacious:', () => {
   describe('Event attaching:', () => {
     it('should instantiate with no events', () => {
-      expect(plex.events()).to.deep.equal({});
-      expect(typeof plex.events('test')).to.equal('undefined');
+      expect(plex.eventFunctions()).to.deep.equal({});
+      expect(typeof plex.eventFunctions('test')).to.equal('undefined');
     });
 
     it('should attach a callback to an event', () => {
       plex.on('test', () => {});
-      expect(Object.keys(plex.events()).length).to.equal(1);
-      expect(typeof plex.events('test')).to.equal('function');
+      expect(Object.keys(plex.eventFunctions()).length).to.equal(1);
+      expect(typeof plex.eventFunctions('test')).to.equal('function');
     });
   });
 
