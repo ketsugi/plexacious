be1de936713fce31187e653877912479b5af5a18
Joel Pan <spamtastic@ketsugi.com>
2016-06-26 22:48:49 +0800

    Add some code to the informal test script

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 const Plexacious = require('./lib/Plexacious');
 const config = require('./config');
 const plex = new Plexacious(config);
+
+plex
+  .on('init', () => {
+    console.log('Init');
+  })
+  .on('mediaAdded', media => {
+    console.log('Media', media.title);
+  });
