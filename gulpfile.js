dfd42a97531b802c478826bc0f4598e0fc280ea2
Joel Pan <spamtastic@ketsugi.com>
2016-06-27 16:24:45 +0800

    Remove babel and add linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 const srcDir = './src';
 const buildDir = './lib';
 
-gulp.task('babelify', () => {
+gulp.task("lint", function () {
   return gulp.src(`${srcDir}/**/*.js`)
-    .pipe(babel())
-    .pipe(gulp.dest(buildDir));
+    .pipe(eslint({
+        extends: 'eslint:recommended',
+        ecmaFeatures: {
+            'modules': true
+        },
+        rules: {
+          'comma-dangle': ['error', 'always-multiline'],
+          'no-console': 'off'
+        },
+        env: {
+          node: true,
+          es6: true
+        }
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('watch', () => {
-  gulp.watch(`${srcDir}/**/*.js`, ['babelify']);
+  gulp.watch(`${srcDir}/**/*.js`, ['lint']);
 });
 
-gulp.task('default', ['babelify', 'watch']);
+gulp.task('default', ['watch']);
