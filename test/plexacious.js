35009fd5f0926520c668c656a488bdbf52ae3611
Joel Pan <spamtastic@ketsugi.com>
2016-07-01 02:37:14 +0800

    Expand mocha tests

--- test/plexacious.js
+++ test/plexacious.js
@@ -6,12 +6,48 @@ const config = {
 }
 
 let plex = new Plexacious();
-plex.setLogLevel('none');
+plex.setLogLevel('error');
 
 describe('Plexacious:', () => {
-  describe('Environment:', () => {
+  describe('Test Environment:', () => {
     it('should read the environment variable correctly', () => {
       expect(config.token).to.not.be.null;
+    });
+  });
+
+  describe('Constructor:', () => {
+    it('should initialize a blank cache', () => {
+      expect(Object.keys(plex.cache)).to.deep.equal(['recentlyAdded', 'servers', 'sessions']);
+      expect(Object.keys(plex.cache.recentlyAdded).length).to.equal(0);
+      expect(Object.keys(plex.cache.servers).length).to.equal(0);
+      expect(Object.keys(plex.cache.sessions).length).to.equal(0);
+    });
+  });
+
+  describe('Initialization:', () => {
+    it('should return the Plexacious object', () => {
+      expect(plex.init(config)).to.equal(plex);
+    });
+  });
+
+  describe('Start', () => {
+    it('should return the Plexacious object', () => {
+      expect(plex.start()).to.equal(plex);
+    });
+    it('should switch on the running flag', () => {
+      expect(plex.running).to.be.true;
+    });
+  });
+
+  describe('Stop', () => {
+    it('should return the Plexacious object', () => {
+      expect(plex.stop()).to.equal(plex);
+    });
+    it('should switch off the running flag', () => {
+      expect(plex.running).to.be.false;
+    });
+    it('should clear the timer', () => {
+      expect(plex._timeoutObj).to.be.undefined;
     });
   });
 });
